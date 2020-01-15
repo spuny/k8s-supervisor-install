ba01808e6a26415cfb92aa6d55773fcce756299d
Jakub Spunar <spuny4@gmail.com>
2020-01-15 18:11:23 +0100

    Repair of PROPT

--- install.sh
+++ install.sh
@@ -58,4 +58,4 @@ echo "Run armada docker container on master-1 or any other master"
 # Install oh-my-zs on the supervisor of cluster
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/plugins\=\(git\)/plugins\=\(git kubectl\)/g' ~/.zshrc
-echo -e "PROMPT='%(!.%{%F{yellow}%}.)\$USER@%{\$fg[white]%}%M \${ret_status} %{\$fg[cyan]%}%c%{$reset_color%} \$(git_prompt_info)'" >> ~/.zshrc
+echo -e "PROMPT='%(!.%{%F{yellow}%}.)\$USER@%{\$fg[white]%}%M \${ret_status} %{\$fg[cyan]%}%c%{\$reset_color%} \$(git_prompt_info) '" >> ~/.zshrc
