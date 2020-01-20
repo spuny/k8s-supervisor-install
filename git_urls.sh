f4bec448ab502aa050c95011442a020d3c9c2862
Jakub Spunar <spuny4@gmail.com>
2020-01-20 14:59:14 +0100

    Changing to stable branch of kubespray

--- git_urls.sh
+++ git_urls.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 os_helm="https://github.com/spuny/openstack-helm.git"
 os_helm_infra="https://github.com/spuny/openstack-helm-infra.git"
+# This is release 2.12
 kubespray="https://github.com/kubernetes-sigs/kubespray.git"
+# This is master
+#kubespray="https://github.com/kubernetes-sigs/kubespray.git"
 
 # Armada manifests are kept in private repo!!!
 # Download and install it manually
