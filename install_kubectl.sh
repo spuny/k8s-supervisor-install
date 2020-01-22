b2e066a847438a07c8b516b185c14bf01bd25525
Jakub Spunar <spuny4@gmail.com>
2020-01-22 16:03:25 +0100

    Now downloading stable version 1.16.3 of kubectl

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Install latest version of kubectl
-curl -sLO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# This one is the latest!
+#curl -sLO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# v 1.16.3
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.3/bin/linux/amd64/kubectl
 sudo mv kubectl /usr/bin/
 sudo chmod 755 /usr/bin/kubectl
 
