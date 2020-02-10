ef2f1f4d0611b81c12005ac18f7e63ca9ad03004
Jakub Spunar <spuny4@gmail.com>
2020-02-10 12:06:46 +0100

    Added private armada repository

--- git_urls.sh
+++ git_urls.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 os_helm="https://github.com/spuny/openstack-helm.git"
 os_helm_infra="https://github.com/spuny/openstack-helm-infra.git"
-# This is release 2.12
 kubespray="https://github.com/kubernetes-sigs/kubespray.git"
-# This is master
-#kubespray="https://github.com/kubernetes-sigs/kubespray.git"
+armada_rep="https://github.com/spuny/armada-manifests.git"
 
 # Armada manifests are kept in private repo!!!
 # Download and install it manually
